d29c93ec9471b9da2f821026ef248c512af9f434
LeonardoMattioli <leonardomattioli00@gmail.com>
2024-03-11 21:34:14 -0300

    findbyId implementantion

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,19 +2,17 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department obj = new Department(1, "books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(21, "bob", "Bob@gmail.com", new Date(), 3000.0, obj);
-        System.out.println(seller);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
